1ea488027fdd9e1eb442e9a8901dd38a237483e9
cmgoulet <cmgoulet@berkeley.edu>
2025-03-24 14:56:41 -0700

    toggle

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ document.addEventListener("DOMContentLoaded", function () {
     faders.forEach(fader => appearOnScroll.observe(fader));
   
     // Dark Mode Toggle
-    const toggle = document.getElementById("theme-toggle");
-    toggle.addEventListener("click", () => {
-      document.body.classList.toggle("dark-mode");
-      toggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
-    });
+    const toggle = document.getElementById("darkModeToggle");
+    toggle.addEventListener("change", () => {
+        document.body.classList.toggle("dark-mode");
+      });
+      
   });
   
